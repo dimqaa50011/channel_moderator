e9f5f6d0ba418c81e1ff0498de9d39e535b8c7a5
Dmitry <dz5011@yandex.ru>
2022-10-15 08:59:50 +0300

    group filter

--- tg_bot/filters/register_filters.py
+++ tg_bot/filters/register_filters.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
 
 from .admin import IsAdmin
+from .group import IsGroup
 
 
 def register_all_filters(dp: Dispatcher):
     dp.filters_factory.bind(IsAdmin)
+    dp.filters_factory(IsGroup)
