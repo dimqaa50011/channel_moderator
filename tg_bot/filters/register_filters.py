dd0ddc853243768ead8fd2cf95ece91cf505cfa5
Dmitry <dz5011@yandex.ru>
2022-10-15 09:03:33 +0300

    privat filter

--- tg_bot/filters/register_filters.py
+++ tg_bot/filters/register_filters.py
@@ -2,8 +2,10 @@ from aiogram import Dispatcher
 
 from .admin import IsAdmin
 from .group import IsGroup
+from .privat import IsPrivat
 
 
 def register_all_filters(dp: Dispatcher):
     dp.filters_factory.bind(IsAdmin)
     dp.filters_factory(IsGroup)
+    dp.filters_factory(IsPrivat)
